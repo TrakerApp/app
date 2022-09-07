948b79d59e57cb026d93e41e269747132f2701e9
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-09-07 12:40:00 -0400

    Working swipeable and list items with theme

--- components/TrackingListItem.js
+++ components/TrackingListItem.js
@@ -1,9 +1,15 @@
+import { useContext } from "react"
 import { View, StyleSheet } from "react-native"
 import { Text } from "react-native-paper"
+import { PreferencesContext } from "../store/context/preferences-context"
 
 export default function TrackingListItem({ name, lastOccurrence }) {
+	const preferencesContext = useContext(PreferencesContext)
+	const backgroundColor = preferencesContext.isThemeDark ? "#010101" : "#f2f2f2"
+	const borderRightColor = preferencesContext.isThemeDark ? "#313131" : "#c8c8c8"
+
 	return (
-		<View style={styles.rootContainer}>
+		<View style={[styles.rootContainer, { backgroundColor, borderRightColor }]}>
 			<Text style={styles.name}>{name}</Text>
 			{lastOccurrence && (<Text style={styles.lastOccurrence}>Last occurence: {lastOccurrence}</Text>)}
 		</View>
@@ -17,8 +23,6 @@ const styles = StyleSheet.create({
 		paddingBottom: 12,
 		justifyContent: 'center',
 		minHeight: 50,
-		backgroundColor: '#f2f2f2',
-		borderRightColor: '#c8c8c8',
 		borderRightWidth: 1,
 	},
 	name: {
