bef18911103acf847b0a311d2c6c43e9a23ac18d
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-09-08 17:13:22 -0400

    TK-14 refactor

--- components/TrackingListItem.js
+++ components/TrackingListItem.js
@@ -1,17 +1,13 @@
-import { useContext } from "react"
 import { View, StyleSheet } from "react-native"
 import { Text } from "react-native-paper"
-import { PreferencesContext } from "../store/context/preferences-context"
+import useColors from "../util/hooks/useColors"
 
 export default function TrackingListItem({ name, lastOccurrenceTime }) {
-	const preferencesContext = useContext(PreferencesContext)
-	const backgroundColor = preferencesContext.isThemeDark ? "#010101" : "#f2f2f2"
-	const borderRightColor = preferencesContext.isThemeDark ? "#313131" : "#c8c8c8"
-
+	const colors = useColors
 	const lastOccurrenceTimeStr = lastOccurrenceTime ? lastOccurrenceTime.toLocaleString() : "Never"
 
 	return (
-		<View style={[styles.rootContainer, { backgroundColor, borderRightColor }]}>
+		<View style={[styles.rootContainer, { backgroundColor: colors.normalBackground, borderRightColor: colors.normalBorder }]}>
 			<Text style={styles.name}>{name}</Text>
 			{lastOccurrenceTimeStr ? (<Text style={styles.lastOccurrence}>Last occurrence: {lastOccurrenceTimeStr}</Text>) : null}
 		</View>
--- util/hooks/useColors.js
+++ util/hooks/useColors.js
@@ -3,13 +3,18 @@ import { PreferencesContext } from "../../store/context/preferences-context";
 
 const LIGHT_COLORS = {
 	helpText500: "#999",
+	normalBackground: "#f2f2f2",
+	normalBorder: "#c8c8c8",
 	modalBackground: "white",
 }
 
 const DARK_COLORS = {
 	helpText500: "#999",
+	normalBackground: "#010101",
 	modalBackground: "#333",
+	normalBorder: "#313131",
 }
+
 
 export default useColors = () => {
 	const { isThemeDark } = useContext(PreferencesContext);
