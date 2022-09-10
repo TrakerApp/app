021bb83121ff815c3ae8351d1e8abd08998a11e5
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-09-09 21:00:10 -0400

    TK-14 added examples to new tracking modal

--- components/NewTrackingForm.js
+++ components/NewTrackingForm.js
@@ -5,31 +5,31 @@ import useColors from "../util/hooks/useColors";
 
 export default function NewTrackingForm({ isEditing, onCreate, onCancel }) {
   const [name, setName] = useState("");
-	const [nameHasError, setNameHasError] = useState(false);
+  const [nameHasError, setNameHasError] = useState(false);
   const nameInputRef = useRef(null);
-	const colors = useColors()
+  const colors = useColors();
 
   const handleCreate = () => {
     if (name !== "") {
-			setNameHasError(false)
+      setNameHasError(false);
       onCreate(name);
       setName("");
     } else {
-			setNameHasError(true)
-		}
+      setNameHasError(true);
+    }
   };
 
   const handleCancel = () => {
     onCancel();
   };
 
-	const handleTextInput = (text) => {
-		setName(text)
+  const handleTextInput = (text) => {
+    setName(text);
 
-		if (text !== "") {
-			setNameHasError(false)
-		}
-	}
+    if (text !== "") {
+      setNameHasError(false);
+    }
+  };
 
   useEffect(() => {
     if (isEditing) {
@@ -38,19 +38,29 @@ export default function NewTrackingForm({ isEditing, onCreate, onCancel }) {
   }, [isEditing]);
 
   return (
-    <View style={[styles.rootContainer, { backgroundColor: colors.modalBackground }]}>
+    <View
+      style={[
+        styles.rootContainer,
+        { backgroundColor: colors.modalBackground },
+      ]}
+    >
       <Text style={styles.title}>Add new tracking</Text>
       <TextInput
         ref={nameInputRef}
-				mode="outlined"
-				error={nameHasError}
+        mode="outlined"
+        error={nameHasError}
         label="Name"
         value={name}
         placeholder="Drink Water"
         onChangeText={handleTextInput}
       />
-      <HelperText type="error" visible={nameHasError}>
-        Name is required
+      {nameHasError && (
+        <HelperText type="error" visible={nameHasError}>
+          Name is required
+        </HelperText>
+      )}
+      <HelperText type="info" visible={true}>
+        You can change this name later
       </HelperText>
       <View style={styles.buttonsContainer}>
         <Button style={styles.button} mode="outlined" onPress={handleCancel}>
@@ -59,6 +69,19 @@ export default function NewTrackingForm({ isEditing, onCreate, onCancel }) {
         <Button style={styles.button} mode="outlined" onPress={handleCreate}>
           Create
         </Button>
+      </View>
+      <View style={styles.examplesContainer}>
+        <Text style={[styles.exampleTitle, { color: colors.softText1 }]}>
+          Enter the name of the action, event or occurrence that you want to
+          track.
+        </Text>
+        <Text style={[styles.exampleTitle, { color: colors.softText1 }]}>Some examples:</Text>
+        <Text style={[styles.exampleInfo, { color: colors.softText1 }]}>Drink water</Text>
+        <Text style={[styles.exampleInfo, { color: colors.softText1 }]}>Stretch</Text>
+        <Text style={[styles.exampleInfo, { color: colors.softText1 }]}>Drink coffee</Text>
+        <Text style={[styles.exampleInfo, { color: colors.softText1 }]}>Wake up</Text>
+        <Text style={[styles.exampleInfo, { color: colors.softText1 }]}>Feeling bored</Text>
+        <Text style={[styles.exampleInfo, { color: colors.softText1 }]}>Called my mom</Text>
       </View>
     </View>
   );
@@ -75,6 +98,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-evenly",
   },
+  examplesContainer: {
+    marginTop: 20,
+  },
   title: {
     textAlign: "center",
     fontSize: 18,
@@ -84,4 +110,13 @@ const styles = StyleSheet.create({
   button: {
     paddingHorizontal: 20,
   },
+  exampleTitle: {
+    marginBottom: 10,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  exampleInfo: {
+    fontSize: 16,
+    textAlign: 'center',
+  }
 });
--- util/hooks/useColors.js
+++ util/hooks/useColors.js
@@ -3,6 +3,7 @@ import { PreferencesContext } from "../../store/context/preferences-context";
 
 const LIGHT_COLORS = {
 	helpText: "#999",
+	softText1: "#333",
 	normalBackground: "#f2f2f2",
 	normalBorder: "#c8c8c8",
 	modalBackground: "white",
@@ -10,6 +11,7 @@ const LIGHT_COLORS = {
 
 const DARK_COLORS = {
 	helpText: "#999",
+	softText1: "#ddd",
 	normalBackground: "#010101",
 	modalBackground: "#333",
 	normalBorder: "#313131",
