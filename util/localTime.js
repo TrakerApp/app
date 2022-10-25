4d8facb8371d3286cb01ea563a74e36c229c36da
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2022-10-25 16:24:22 -0300

    TK-38 fix date wrongly formatted on android

--- util/localTime.js
+++ util/localTime.js
@@ -1,8 +1,8 @@
+import moment from "moment";
+
 export const localTime = (timestamp) => {
 	if (!timestamp) {
 		return "Never";
 	}
-	const date = new Date(timestamp);
-	const options = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
-	return date.toLocaleDateString("en-US", options);
+	return moment(timestamp).format("MMMM Do YYYY, hh:mm:ss a");
 }
